045cfcb5b6a0812a864517c0ecfb04b395bf4e49
Dayron-z <zbastian.zapata@gmail.com>
2024-07-23 10:55:52 -0500

    Cuarto microservicio

---
+++ microservice-class/src/main/java/com/example/demo/MicroserviceClassApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceClassApplication.class, args);
+	}
+
+}
